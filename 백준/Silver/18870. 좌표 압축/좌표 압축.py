10fa345d48e7aa045fb1a852591fd75c7fe1f920
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-03-25 21:33:33 +0900

    [Silver II] Title: 좌표 압축, Time: 2192 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = list(map(int, input().split()))
+n_arr = sorted(list(set(arr)))
+dic = {}
+for i in range(len(n_arr)):
+    dic[n_arr[i]] = i
+
+for j in arr:
+    print(dic[j], end = ' ')
