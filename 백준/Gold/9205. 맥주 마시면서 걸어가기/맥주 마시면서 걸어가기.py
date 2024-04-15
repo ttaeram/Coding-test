790545f18034b6fa5906549831bf091c228d7441
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-15 19:36:04 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    check = [[] for _ in range(N + 2)]
+    conv = [list(map(int, input().split())) for _ in range(N + 2)]
+    for n in range(N + 2):
+        p0 = conv[n][0]
+        p1 = conv[n][1]
+        for i in range(N + 2):
+            h0 = conv[i][0]
+            h1 = conv[i][1]
+            if abs(h0 - p0) + abs(h1 - p1) <= 20 * 50:
+                check[n].append(i)
+
+    visited = [0] * (N + 2)
+    stack = [0]
+    flag = False
+    while stack:
+        pos = stack.pop()
+        if not visited[pos]:
+            visited[pos] = 1
+            for j in check[pos]:
+                stack.append(j)
+                if j == N + 1:
+                    ans = 'happy'
+                    flag = True
+                    break
+    if not flag:
+        ans = 'sad'
+    print(ans)
