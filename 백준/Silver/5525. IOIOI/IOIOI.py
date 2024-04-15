0f79dbc93730cb8e7c33818830eb515d54a241a0
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-15 20:58:30 +0900

    [Silver I] Title: IOIOI, Time: 300 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,10 +5,22 @@ N = int(input())
 M = int(input())
 S = input()
 s = 'I'
-for _ in range(N):
-    s += 'OI'
-cnt = 0
-for m in range(M):
-    if S[m:m + (2 * N + 1)] == s:
-        cnt += 1
-print(cnt)
+
+def sol(N, M, S):
+    i = 0
+    cnt = 0
+    ans = 0
+    while i < (M - 1):
+        if S[i : i + 3] == 'IOI':
+            i += 2
+            cnt += 1
+            if cnt == N:
+                ans += 1
+                cnt -= 1
+        else:
+            i += 1
+            cnt = 0
+    return ans
+
+ans = sol(N, M, S)
+print(ans)
