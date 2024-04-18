fda99b811db975f3413033a1784fe656e1180fa2
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-18 21:17:29 +0900

    [level 3] Title: 야근 지수, Time: 749.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -11,11 +11,6 @@ def solution(n, works):
         work = heapq.heappop(heap)[1]
         work -= 1
         heapq.heappush(heap, (-1 * work, work))
-
-    # works.sort()
-    # for _ in range(n):
-    #     works[-1] -= 1
-    #     works.sort()
     ans = 0
     for w in heap:
         ans += w[1] ** 2
