0063dcf5a1a829d88caadf33a814091b779c8180
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-26 15:20:22 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,50 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-sadari = {}
-sadari_start = []
-for _ in range(N):
-    a, b = map(int, input().split())
-    sadari[a] = b
-    sadari_start.append(a)
-bam = {}
-bam_start = []
-bam_end = [1]
-for _ in range(M):
-    a, b = map(int, input().split())
-    bam[a] = b
-    bam_start.append(a)
-    bam_end.append(b)
+from heapq import heappop, heappush
 
-map = [0] * 101
-for idx in bam_start:
-    map[idx] = 100
-def sol(n):
-    for i in range(n, 101):
-        if i in bam_start:
-            continue
-        for j in range(i + 1, i + 7):
-            if j <= 100:
-                if j in bam_start:
-                    if map[bam[j]] != 0:
-                        map[bam[j]] = min(map[bam[j]], map[i] + 1)
-                    else:
-                        map[bam[j]] = map[i] + 1
-                if j in sadari_start:
-                    if map[j] > 0:
-                        map[j] = min((map[i] + 1), map[j])
-                        map[sadari[j]] = map[j]
-                    else:
-                        map[j] = (map[i] + 1)
-                        map[sadari[j]] = map[j]
-                else:
-                    if map[j] > 0:
-                        map[j] = min((map[i] + 1), map[j])
-                    else:
-                        map[j] = map[i] + 1
+g = [[] for _ in range(101)]
+n, m = map(int, input().rstrip().split())
+for _ in range(n+m):
+    k1, k2 = map(int, input().rstrip().split())
+    g[k1].append((0, k2))
+for i in range(101):
+    if not g[i]:
+        g[i].extend([(1, i+k) for k in range(1, min(7, 101-i))])
 
-bam_end.sort()
-for n in bam_end:
-    sol(n)
-print(map[100])
+d = [float('inf')]*101
+q = [(0, 1)]
+while q:
+    cw, cp = heappop(q)
+    if cp != 100 and d[cp] >= cw:
+        for nw, np in g[cp]:
+            nw += cw
+            if d[np] > nw:
+                d[np] = nw
+                heappush(q, (nw, np))
+print(d[100])
