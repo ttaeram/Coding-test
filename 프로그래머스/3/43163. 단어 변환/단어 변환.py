c667eb7cc5a336c326825f7ecc9931c311156065
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-28 17:39:03 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    else:
+        stack = deque()
+        stack.append((begin, 0))
+        while stack:
+            word, cnt = stack.popleft()
+            if word == target:
+                return cnt
+            for wo in words:
+                cntt = 0
+                for i in range(len(wo)):
+                    if wo[i] != word[i]:
+                        cntt += 1
+                if cntt == 1:
+                    stack.append((wo, cnt + 1))
