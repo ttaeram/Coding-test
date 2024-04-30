e1c6d6383e73be24a982720fe34a9b05a02a525b
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-04-30 16:19:43 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 7736 ms, Memory: 121336 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def sol():
+    N = int(input())
+    arr = []
+    ans = 0
+    for _ in range(N):
+        a, b = map(int, input().split())
+        arr.append((a, b))
+        
+    arr.sort(key=lambda x: x[0])
+    stack = []
+    for a, b in arr:
+        if not stack:
+            ans += 1
+            stack.append((a, b))
+        else:
+            flag = True
+            for i in range(len(stack)):
+                if stack[i][1] <= a:
+                    stack[i] = (a, b)
+                    flag = False
+                    break
+            if flag:
+                ans += 1
+                stack.append((a, b))
+    print(ans)
+sol()
