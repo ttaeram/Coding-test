ada8db4096e5ed3578d6d6aae99338a3169d4cdb
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-05-02 21:19:50 +0900

    [Silver I] Title: 양, Time: 180 ms, Memory: 37580 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -2,6 +2,25 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
+def bfs(r, c):
+    cnt_v = 0
+    cnt_o = 0
+    stack = deque([(r, c)])
+    while stack:
+        x, y = stack.popleft()
+        if (x, y) not in check:
+            check.add((x, y))
+            if arr[x][y] == 'v':
+                cnt_v += 1
+            elif arr[x][y] == 'o':
+                cnt_o += 1
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < R and 0 <= ny < C and arr[nx][ny] != '#':
+                    stack.append((nx, ny))
+    return(cnt_o, cnt_v)
+
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
@@ -16,22 +35,7 @@ for r in range(R):
     for c in range(C):
         if arr[r][c] == '.' or arr[r][c] == 'v' or arr[r][c] == 'o':
             if (r, c) not in check:
-                cnt_v = 0
-                cnt_o = 0
-                stack = deque([(r, c)])
-                while stack:
-                    x, y = stack.popleft()
-                    if (x, y) not in check:
-                        check.add((x, y))
-                        if arr[x][y] == 'v':
-                            cnt_v += 1
-                        elif arr[x][y] == 'o':
-                            cnt_o += 1
-                        for i in range(4):
-                            nx = x + dx[i]
-                            ny = y + dy[i]
-                            if 0 <= nx < R and 0 <= ny < C and arr[nx][ny] != '#':
-                                stack.append((nx, ny))
+                cnt_o, cnt_v = bfs(r, c)
                 if cnt_o > cnt_v:
                     ans_o += cnt_o
                 else:
