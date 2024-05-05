975d1e3e80e972791add713050155214860e6cef
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-05-05 23:43:55 +0900

    [Silver II] Title: N번째 큰 수, Time: 1752 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for i in range(N):
+    for j in map(int, input().split()):
+        heapq.heappush(arr, j)
+        if len(arr) > N:
+            heapq.heappop(arr)
+
+    ans = arr[0]
+print(ans)
