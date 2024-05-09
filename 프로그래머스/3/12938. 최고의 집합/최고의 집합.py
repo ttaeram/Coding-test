7c498a3fda72f109f2e53a0b199c4f93522aaae2
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-05-09 17:16:59 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -0,0 +1,15 @@
+def solution(n, s):
+    if s < n:
+        ans = [-1]
+        return ans
+    res = s // n
+    etc = s % n
+    ans = [res] * n
+    if etc:
+        for i in range(etc):
+            ans[i] += 1
+        ans.sort()
+        return ans
+    else:
+        ans.sort()
+        return ans
