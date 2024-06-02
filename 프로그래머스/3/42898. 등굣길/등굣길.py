7726bd007aec238a60d8e1f7eb23c0015ff3bc1b
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2024-06-02 15:57:00 +0900

    [level 3] Title: 등굣길, Time: 2.71 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,21 @@
+def solution(m, n, puddles):
+    arr = [[0] * m for _ in range(n)]
+    oong = set()
+    for i in range(len(puddles)):
+        a, b = puddles[i][0] - 1, puddles[i][1] - 1
+        oong.add((b, a))
+    arr[0][0] = 1
+    for r in range(n):
+        for c in range(m):
+            if r == 0 and c == 0:
+                continue
+            if (r, c) in oong:
+                arr[r][c] = 0
+            else:
+                if r == 0:
+                    arr[r][c] = arr[r][c - 1]
+                elif c == 0:
+                    arr[r][c] = arr[r - 1][c]
+                else:
+                    arr[r][c] = arr[r][c - 1] + arr[r - 1][c]
+    return arr[n - 1][m - 1] % 1000000007
