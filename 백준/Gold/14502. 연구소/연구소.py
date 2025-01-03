14c1465977ada8af1aae777e0bdb3f3820576e6d
Tae Ram <155536387+ttaeram@users.noreply.github.com>
2025-01-03 17:42:19 +0900

    [Gold IV] Title: 연구소, Time: 2132 ms, Memory: 122876 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,55 @@
+from collections import deque
+import copy
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+def sol():
+    Q = deque()
+    semi = copy.deepcopy(arr)
+    for i in range(N):
+        for j in range(M):
+            if semi[i][j] == 2:
+                Q.append((i, j))
+    
+    while Q:
+        r, c = Q.popleft()
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if 0 <= nr < N and 0 <= nc < M:
+                if semi[nr][nc] == 0:
+                    semi[nr][nc] = 2
+                    Q.append((nr, nc))
+
+    global ans
+    cnt = 0
+
+    for i in range(N):
+        cnt += semi[i].count(0)
+    
+    ans = max(ans, cnt)
+
+def wall(cnt):
+    if cnt == 3:
+        sol()
+        return
+    
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                arr[i][j] = 1
+                wall(cnt + 1)
+                arr[i][j] = 0
+
+ans = 0
+wall(0)
+
+print(ans)
